585bd547b4a870d189e6d9d8a624f45abbed916b
rutuja <rutujafeb20@gmail.com>
2024-09-04 16:36:33 +0530

    day3

---
+++ Assignment3/program2.java
@@ -0,0 +1,7 @@
+class Number{
+	public static void main(String[] args){
+	int a;
+	for(a=1;a<=4;a++){
+	 System.out.print(a*3+" ");
+	}}
+}	
---
+++ Assignment3/program3.java
@@ -0,0 +1,12 @@
+class Number{
+	public static void main(String[] args){
+	int a;
+	for(a=1;a<=4;a++){
+		if(a%2==1){
+			System.out.print("0 ");
+	}else{
+		System.out.print("1 ");
+	}
+	}
+	}
+}
