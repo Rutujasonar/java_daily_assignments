9ac505477905a1d250f47827a3aafa7dbaa0f0dd
rutuja <rutujafeb20@gmail.com>
2024-10-05 22:52:32 +0530

    star pattern

--- Assignment7/program2.java
+++ Assignment7/program2.java
@@ -1,13 +1,15 @@
-class pattern{
+class Demo{
 	public static void main(String[] args){
-	for(int i=4;i>=1;i--){
-		for(int j=1;j<i;j++){
-			System.out.print(" ");	
+	int n=4;
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=n-i;j++){
+			System.out.print(" ");
 		}
-		for(int j=0;j<=4-i;j++){
+		for(int j=1;j<=i;j++){
 			System.out.print("*");
 		}
-		System.out.println();
+		System.out.println(" ");
+			
 	}
-}	
-}																					
+	}
+}
