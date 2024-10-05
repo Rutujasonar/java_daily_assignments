6dcdde7d77470309599b8a07f6c99637785905bc
rutuja <rutujafeb20@gmail.com>
2024-10-05 23:51:25 +0530

    pattern

---
+++ Assignment8/program1.java
@@ -0,0 +1,16 @@
+class Demo{
+	public static void main(String[] args){
+	int n=4;
+	int count=0;
+	
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=i;j++){
+			
+			count++;
+			System.out.print(" "+count*count);
+			
+		}
+	System.out.println();
+	}
+	}
+}
---
+++ Assignment8/program3.java
@@ -0,0 +1,11 @@
+class Demo{
+	public static void main(String[] args){
+	int n=4;
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=i;j++){
+			System.out.print(" 01");
+		}
+		System.out.println();
+	}
+	}
+}
